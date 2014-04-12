060ff8bda3ed288640c545f852946ee5000d3ea5
palamccc <palam.c@gmail.com>
2014-04-12 14:37:05 +0530

    added livereload.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,11 @@
-'use strict';
-var path = require('path');
+"use strict";
+var path = require("path");
 
 module.exports = function(grunt) {
+  require('load-grunt-tasks')(grunt);
   grunt.initConfig({
     //specify the output directory
-    outputDir: 'Library-Output',
+    outputDir: "Library-Output",
     //clean all the output files
     clean: {
       all: {
@@ -17,7 +18,7 @@ module.exports = function(grunt) {
     website_generator: {
       all: {
         options: {
-          convertScript : 'convert.js',  //this script will be called for every input file
+          convertScript : "convert.js",  //this script will be called for every input file
           stopOnError : true             //stop grunt if any conversion fails
         },
         files: [{ 
@@ -26,27 +27,40 @@ module.exports = function(grunt) {
           src: ["**/*.{txt,html}"],
           dest: '<%=outputDir%>',
           rename: function(dest, inputfile){
-            //map input txt filename to output index.html filename
+            //map input txt filename to output index.htm filename
             //take input file path, delete extension with regex, add index.htm to the end 
-            return path.join(dest, inputfile.replace(/\.[^\.\/\\]+$/,''), 'index.htm');
+            return path.join(dest, inputfile.replace(/\.[^\.\/\\]+$/,""), "index.htm");
           }
         }]
       }
     },
-    //watch and run generator automatically
     watch: {
+      //watch input files and run generator automatically
       website_generator: {
         files: ["Library/**/*.{txt,html}"],
         tasks: ['newer:website_generator:all']
+      },
+      //watch output files and trigger connect server to reload
+      livereload: {
+        files: [ '<%=outputDir%>/**/*.{html,htm}' ],
+        options: { livereload: true }
       }
+    },
+    //connect webserver to serve files
+    connect: {
+      livereload: {
+        options: {
+          port: 9000,
+          open: true,
+          hostname: "localhost",
+          base: "<%=outputDir%>"
+        }
+      },
     }
   });
-
-  grunt.loadTasks('tasks'); //loads tasks defined in tasks directory
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-newer');
-  grunt.registerTask('build', ['newer:website_generator:all']);
-  grunt.registerTask('rebuild', ['clean:all', 'website_generator:all']);
-  grunt.registerTask('default', ['build']);
+  grunt.loadTasks("tasks"); //loads tasks defined in tasks directory
+  grunt.registerTask("build", ["newer:website_generator:all"]);
+  grunt.registerTask("rebuild", ["clean:all", "website_generator:all"]);
+  grunt.registerTask("default", ["build"]);
+  grunt.registerTask("live", ["connect:livereload", "watch"]);
 };
